da6976f59530539a7a0caa4c3801d375edbdd3da
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-09 18:20:48 +0000

    Added double quotes and edited linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./common/header/header.component";
 
 
 @NgModule({
