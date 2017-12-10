4a4acbfde21cb60ac446543e5b352b0e055ebef8
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-10 21:45:56 +0000

    made consistent single quotes to double quotes

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  selector: "app-search-page",
+  templateUrl: "./search-page.component.html",
+  styleUrls: ["./search-page.component.scss"]
 })
 export class SearchPageComponent implements OnInit {
 
