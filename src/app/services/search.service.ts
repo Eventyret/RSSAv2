fcdb23a35c0ce0ae3d790a610aa591c87a40c2f7
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-11 21:14:51 +0000

    Added variables and injected Http component

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class SearchService {
+	/**
+	 * Variables for
+	 */
+	private query: string;
+	private imdbID: string;
+	private OMDB_KEY: string = environment.OMDB_KEY;
+	private OMDB_URL: string = environment.OMDB_URL;
+	private OMDB_STRING: string = "&apikey=" + this.OMDB_KEY;
+	private RADARR_KEY: string = environment.RADARR_KEY;
+	private RADAR_URL: string = environment.RADARR_URL;
+	private RADAR_STRING: string = "?apikey=" + this.RADARR_KEY;
 
-  constructor() { }
+	constructor(private http: Http) {}
 
+	private handleError(error: Response) {
+		console.log(error);
+		return Observable.throw(error);
+	}
+
+	onlineSearch(query) {
+		return this.http
+			.get(
+				this.OMDB_URL + "?s=" + query + this.OMDB_STRING + "&type=movie"
+			)
+			.map(res => res.json());
+	}
 }
