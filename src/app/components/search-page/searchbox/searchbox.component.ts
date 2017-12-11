3d3f4a119d592f6964a171d30ea4c4317ac9725c
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-11 20:43:26 +0000

    Added SearchFunction to service

--- src/app/components/search-page/searchbox/searchbox.component.ts
+++ src/app/components/search-page/searchbox/searchbox.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
 	selector: "search-box",
@@ -8,9 +8,24 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class SearchBoxComponent implements OnInit {
 	form = new FormGroup({
-		search: new FormControl()
+		search: new FormControl("", Validators.required)
 	});
+
+	get search() {
+		return this.form.get("search");
+	}
 	constructor() {}
+
+	searchMovies(query: string) {
+		return this.searchService.getMovies(query).subscribe(
+			data => {
+				this.handleSuccess(data);
+			},
+			error => {
+				throw error;
+			}
+		);
+	}
 
 	ngOnInit() {}
 }
