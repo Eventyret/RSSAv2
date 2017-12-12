8027bd35ae3c2c75d68e98573e9a66425a50751b
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-12 19:27:31 +0000

    Added function to get the results and map them

--- src/app/services/radarr.service.ts
+++ src/app/services/radarr.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import { environment } from "./../../environments/environment";
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Injectable()
 export class RadarrService {
+	private RADARR_KEY: string = environment.RADARR_KEY;
+	private RADAR_URL: string = environment.RADARR_URL;
+	private RADAR_STRING: string = "?apikey=" + this.RADARR_KEY;
 
-  constructor() { }
+	constructor(private http: Http) {}
 
+	getResultsFromRadarr(){
+		return this.http.get(this.RADAR_URL + this.RADAR_STRING)
+		.map(res => res.json());
+	}
 }
